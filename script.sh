2596329f6cb81cea603e4242c3babeeab7ee7cd4
USB-Coffee <vl.spamblock@gmail.com>
2021-09-28 19:38:25 +0100

    az

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#az storage blob list -c mycontainer --account-name storageaccount > list.json
 echo "name,size,modified date" > output.csv
 cat list.json | jq -r '.[].name' > name.txt
 cat list.json | jq -r '.[].properties.contentLength' > size.txt
