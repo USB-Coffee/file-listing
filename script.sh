4dfb4cb68916bf7fa554dbcd8c88e5d5f40269e3
USB-Coffee <vl.spamblock@gmail.com>
2021-09-28 20:44:30 +0100

    quote values in csv and merge into excel sheet

--- script.sh
+++ script.sh
@@ -2,9 +2,10 @@
 #az storage blob list -c mycontainer --account-name mystorageaccount > mycontainer.json
 for i in *.json; do
 	echo "name,size,modified date" > "${i%.*}".csv
-	cat "$i" | jq -r '.[].name' > name.txt
-	cat "$i" | jq -r '.[].properties.contentLength' > size.txt
-	cat "$i" | jq -r '.[].properties.lastModified' > date.txt
+	cat "$i" | jq '.[].name' > name.txt
+	cat "$i" | jq '.[].properties.contentLength' > size.txt
+	cat "$i" | jq  '.[].properties.lastModified' > date.txt
 	paste -d "," name.txt size.txt date.txt >> "${i%.*}".csv
+	ssconvert --merge-to=combined.xls *.csv
 	rm name.txt size.txt date.txt
 done
