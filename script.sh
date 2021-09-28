182b3fa7b8aa567f54131d951b63d9e3ed317eda
USB-Coffee <vl.spamblock@gmail.com>
2021-09-28 20:47:59 +0100

    quote headers in csv's

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# ssconvert provided by gnumeric package (GUI) 
 #az storage blob list -c mycontainer --account-name mystorageaccount > mycontainer.json
 for i in *.json; do
-	echo "name,size,modified date" > "${i%.*}".csv
+	echo "\"name\",\"size\",\"modified date\"" > "${i%.*}".csv
 	cat "$i" | jq '.[].name' > name.txt
 	cat "$i" | jq '.[].properties.contentLength' > size.txt
 	cat "$i" | jq  '.[].properties.lastModified' > date.txt
