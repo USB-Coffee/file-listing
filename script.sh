9893f1501bb8832298ee43986172faf27b49738c
USB-Coffee <vl.spamblock@gmail.com>
2021-09-28 19:30:03 +0100

    ads

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "name,size,modifieid date" > output.csv
+cat list.json | jq -r '.[].name' > name.txt
+cat list.json | jq -r '.[].properties.contentLength' > size.txt
+cat list.json | jq -r '.[].properties.lastModified' > date.txt
+paste -d "," name.txt size.txt date.txt >> output.csv
+rm name.txt size.txt date.txt
